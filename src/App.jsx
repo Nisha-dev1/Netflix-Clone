3a1b3a713d5ad52521f36eaf2afef0192541934c
Nisha <nishusahibganj1122@gmail.com>
2025-07-03 12:12:22 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Home from "./pages/Home/Home"
+import TitleCards from './components/TitleCards/TitleCards'
+
+const App = () => {
+  return (
+    <div>
+      <Home/>
+    </div>
+  )
+}
+
+export default App
