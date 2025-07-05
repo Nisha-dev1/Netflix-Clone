93dad7cf898f0f978ba949b44f8c1244ddb81f3b
Nisha <nishusahibganj1122@gmail.com>
2025-07-05 22:55:20 +0530

    added sign in and sign up page

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, {useRef } from 'react'
 import "./TitleCards.css"
+import cards_data from "../../assets/cards/Cards_data"
 
-const TitleCards = () => {
+
+const TitleCards = ({title}) => {
+
+const cardsRef = useRef();
+
+
   return (
-    <div>
-      
+    <div className='title-cards'>
+      <h2>{title?title:"Popular on Netflix"}</h2>
+      <div className="card-list" ref={cardsRef}>
+        {cards_data.map(
+          (card, index) => {
+            return <div className='card' key={index}>
+              <img src= {card.image} alt="" />
+              <p>{card.name}</p>
+            </div>
+          })}
+      </div>
     </div>
   )
 }
